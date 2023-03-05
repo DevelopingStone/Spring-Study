bba25de2669c34a02ab6c18f76be440ebc03657e
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-05 20:26:24 +0900

    Java Project

---
+++ src/main/java/com/knight/api/RetrofitService.java
@@ -0,0 +1,12 @@
+package com.knight.api;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RetrofitService {
+
+  @GET("/api/users/")
+  Call<Object> getUsers(@Query("page") int page);
+
+}
