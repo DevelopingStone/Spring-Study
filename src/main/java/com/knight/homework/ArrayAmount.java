1ee3207d0620b3aabc75a228516133111ccc2eb9
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-06 17:10:19 +0900

    23-03-06 Commit

---
+++ src/main/java/com/knight/homework/ArrayAmount.java
@@ -0,0 +1,23 @@
+package com.knight.homework;
+
+public class ArrayAmount {
+
+  public static void main(String[] args) {
+
+    int[] array = {1,2,3,4,5,6,7,8,9,10};
+
+    int[] arrayAmount = new int[20];
+    arrayAmount = array;
+    array = new int[20];
+    array = arrayAmount;
+
+
+
+    for( int i : array){
+      System.out.println(i);
+    }
+
+
+  }
+
+}
