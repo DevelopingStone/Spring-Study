49dc68a26de515880ebc7f1e4b089be26b426296
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-06 16:58:00 +0900

    23-03-06 Commit

---
+++ src/main/java/com/knight/homework/ArrayInstance.java
@@ -0,0 +1,29 @@
+package com.knight.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayInstance {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    List<Integer> array = new ArrayList<>();
+
+    for (int i = 0; i < array.size() + 1; i++) {
+      Integer number = scan.nextInt();
+      array.add(number);
+
+      System.out.println(array.get(i));
+    }
+
+
+  }
+
+
+}
+
+
+
+
