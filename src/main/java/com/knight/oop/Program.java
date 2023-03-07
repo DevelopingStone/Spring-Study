00ca7f9c144ee374c1d2052a41b16ebefbf598a8
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-07 23:57:55 +0900

    23-03-07 Commit

--- src/main/java/com/knight/oop/Program.java
+++ src/main/java/com/knight/oop/Program.java
@@ -4,22 +4,9 @@ public class Program {
 
   public static void main(String[] args) {
 
-
-    ExamList list = new ExamList();
-
-    ExamList.inputList(list);
-
-    list.inputList(list);
-
-
   }
 
 }
 
-class ExamList{
 
-  static void inputList(ExamList list){
 
-  }
-
-}
