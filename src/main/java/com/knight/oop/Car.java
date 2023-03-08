6574470de5bfbb116d998064cc389421981761e9
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-08 18:00:00 +0900

    23-03-07 Commit

---
+++ src/main/java/com/knight/oop/Car.java
@@ -0,0 +1,20 @@
+package com.knight.oop;
+
+class Car {
+
+  private String make;
+  private String model;
+
+  public Car(String make, String model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  public String getMake() {
+    return make;
+  }
+
+  public String getModel() {
+    return model;
+  }
+}
