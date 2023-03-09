ff5f974ea43eb441c5e77e3ee70d32de32457675
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-09 13:29:29 +0900

    23-03-09 Commit

---
+++ src/main/java/com/knight/oop/OverRideQuiz.java
@@ -0,0 +1,5 @@
+package com.knight.oop;
+
+public class OverRideQuiz {
+
+}
--- src/main/java/com/knight/oop/Refer.java
+++ src/main/java/com/knight/oop/Refer.java
@@ -5,6 +5,7 @@ public class Refer {
 
   public void test0() {
     System.out.println("부모클래스 test 출력 / 부모클래스만 구현");
+    test1();
   }
 
   public void test1() {
@@ -19,10 +20,13 @@ class NewRefer extends Refer {
 
   public static void main(String[] args) {
     Refer refer = new NewRefer();
+//    refer.test0();
+//    refer.test1();
+//    NewRefer referExam = (NewRefer) refer;
+//    referExam.test2();
+//    ((NewRefer)refer).test2();
+
     refer.test0();
-    refer.test1();
-    NewRefer referExam = (NewRefer) refer;
-    referExam.test2();
 
 
   }
@@ -30,6 +34,7 @@ class NewRefer extends Refer {
 
   public void test1() {
     System.out.println("자식클래스 test1 출력 / 부모클래스도 구현");
+    this.test0();
   }
 
   public void test2() {
