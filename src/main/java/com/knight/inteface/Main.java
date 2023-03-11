abab6a06f4e5670679e1e22235c4cb5b40fe9e4e
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-11 20:21:13 +0900

    23-03-11 Commit

--- src/main/java/com/knight/inteface/Main.java
+++ src/main/java/com/knight/inteface/Main.java
@@ -1,30 +1,20 @@
 package com.knight.inteface;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args)
-      throws Exception {
-    String fileName = "src/main/java/com/knight/inteface/setting.txt";
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(
+        new FileReader("src/main/java/com/knight/inteface/setting.txt"));
+    String className = br.readLine();
+    Class clazz = Class.forName(className);
+    Inter cClazz = (Inter) clazz.getDeclaredConstructor().newInstance();
 
-    BufferedReader br = new BufferedReader(new FileReader(fileName));
-    System.out.println(br);
-    String line = br.readLine();
-    System.out.println(line);
-    Class clazz = Class.forName(line);
     A a = new A();
-    Inter inter = (Inter)clazz.newInstance();
-    a.print(inter);
-
-
+    a.print(cClazz);
 
 
   }
-
 }
