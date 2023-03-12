adb136c2f91599d1b8b04f01828425daa4aa8318
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-12 17:14:24 +0900

    23-03-12 Commit

---
+++ src/main/java/com/knight/excep/WhatExcep.java
@@ -0,0 +1,11 @@
+package com.knight.excep;
+
+public class WhatExcep {
+
+  public static void main(String[] args) {
+    System.out.println("테스트");
+  }
+
+
+
+}
--- src/main/java/com/knight/inteface/Main.java
+++ src/main/java/com/knight/inteface/Main.java
@@ -5,16 +5,34 @@ import java.io.FileReader;
 
 public class Main {
 
+//  public static void main(String[] args) throws Exception {
+//    BufferedReader br = new BufferedReader(
+//        new FileReader("src/main/java/com/knight/inteface/setting.txt"));
+//    String className = br.readLine();
+//    Class clazz = Class.forName(className);
+//    Inter cClazz = (Inter) clazz.newInstance();
+//
+//    A a = new A();
+//    a.print(cClazz);
+//
+//
+//  }
+
   public static void main(String[] args) throws Exception {
+
     BufferedReader br = new BufferedReader(
         new FileReader("src/main/java/com/knight/inteface/setting.txt"));
-    String className = br.readLine();
-    Class clazz = Class.forName(className);
-    Inter cClazz = (Inter) clazz.getDeclaredConstructor().newInstance();
 
+    String className = br.readLine();
+
+    Class bClazz = Class.forName(className);
+
+    Inter b = (Inter) bClazz.newInstance();
     A a = new A();
-    a.print(cClazz);
+    a.print(b);
 
 
   }
+
+
 }
