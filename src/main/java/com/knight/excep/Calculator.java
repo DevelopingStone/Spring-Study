f2dff9e543243664444b60510c0db2e8604451d4
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-12 22:22:27 +0900

    23-03-12 Commit

--- src/main/java/com/knight/excep/Calculator.java
+++ src/main/java/com/knight/excep/Calculator.java
@@ -2,20 +2,20 @@ package com.knight.excep;
 
 public class Calculator {
 
-  public int plus(int a, int b) throws Exception_Knight {
+  public int plus(int a, int b) throws Exception_Plus {
     int result = a + b;
     if (result > 200) {
-      throw new Exception_Knight();
+      throw new Exception_Plus();
     }
 
     return a + b;
 
   }
 
-  public int subtract(int a, int b) throws Exception_Knight {
+  public int subtract(int a, int b) throws Exception_Subtract {
     int result = a - b;
     if (result < 0) {
-      throw new Exception_Knight();
+      throw new Exception_Subtract();
     }
     return a - b;
 
--- src/main/java/com/knight/excep/Exception_Knight.java
+++
@@ -1,5 +0,0 @@
-package com.knight.excep;
-
-public class Exception_Knight extends Exception{
-
-}
