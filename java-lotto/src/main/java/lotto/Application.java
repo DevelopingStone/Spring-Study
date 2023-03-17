e964181891659a99090423518360bd90895782b2
030831 <kdhc777@mail.ulsan.ac.kr>
2023-03-17 19:58:39 +0900

    2

--- java-lotto/src/main/java/lotto/Application.java
+++ java-lotto/src/main/java/lotto/Application.java
@@ -9,11 +9,28 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Lotto lotto = new Lotto();
 
-        lotto.Play();
+
+        InputView inputView = new InputView();
+
+        OutputView outputView = new OutputView();
+
+        inputView.buyLotto();
+        lotto.extractLottoNumbers(lotto.buyLotto , inputView.money);
+        outputView.viewBuyLotto(lotto.buyLotto);
+
+        inputView.inputPrizeNumbers(lotto.prizeNumber);
+        inputView.inputLottoBonusNumber();
+
+        lotto.getLottoResult(lotto.lottoPrize, lotto.buyLotto , lotto.prizeNumber , inputView.bonusNumber);
+        lotto.getProfitRate(lotto.lottoPrize );
+        System.out.println(Arrays.toString(lotto.lottoPrize));
+        outputView.viewLottoResult(lotto.lottoPrize, lotto.profitRate, inputView.money);
+
 
     }
+
 }
--- java-lotto/src/main/java/lotto/Lotto.java
+++ java-lotto/src/main/java/lotto/Lotto.java
@@ -1,108 +1,148 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class Lotto {
-    private final List<List<Integer>> numbers = new ArrayList<>(); // 지울것.
-    private int money;
-    private List<Integer> select_numbers = new ArrayList<>();
-    private final int[] ranking = new int[8];
+// Ctrl+Alt+L = 자동코드 정리
 
-    private void setNumbers() {
+
+enum Prize {
+    ZERO(0), ONE(0), TWO(0), THREE(5000), FOUR(50_000), FIVE(1_500_000), SIX(
+        2_000_000_000), FIVE_AND_BONUS(
+        30_000_000);
+
+    private final int lottoPrizeValue;
+
+    Prize(int lottoPrizeValue) {
+        this.lottoPrizeValue = lottoPrizeValue;
+    }
+
+    public int getValue() {
+        return lottoPrizeValue;
+    }
+
+}
+
+class Lotto {
+
+    int[] lottoPrize = new int[Prize.values().length]; // 초기값은 0 , Prize 의 개수만큼 배열크기를 유연하게 할당한다.
+    List<List<Integer>> buyLotto = new ArrayList<>(); // 구매한 로또
+    List<Integer> prizeNumber = new ArrayList<>(); // 당첨번호
+    float profitRate = 0;
+
+    public void test() {
+        System.out.println(Arrays.stream(Prize.values())
+            .filter(prize -> prize.ordinal() == 4).collect(Collectors.toList()).get(0).getValue());
+    }
+
+    private int getPrizeIndex(Prize prize) {
+        return lottoPrize[prize.ordinal()];
+    }
+
+    public void getProfitRate(int[] lottoPrize) {
+        for (int i = 0; i < lottoPrize.length; i++) {
+            int finalI = i;
+            profitRate += lottoPrize[i] * Arrays.stream(Prize.values())
+                .filter(prize -> prize.ordinal() == finalI).collect(Collectors.toList()).get(0)
+                .getValue();
+
+        }
+    }
+
+    public void validate(List<Integer> prizeNumber) {
+        if (prizeNumber.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void extractLottoNumbers(List<List<Integer>> buyLotto, int money) {
         for (int i = 0; i < money / 1000; i++) {
-            numbers.add(getNumbers());
+            buyLotto.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         }
     }
 
-    private void validate() {
-        if (select_numbers.size()!=7) {
-            System.out.println("[ERROR] 로또 번호 개수는 7개어야 합니다.");
-            throw new IllegalArgumentException();
+    public void getLottoResult(int[] lottoPrize, List<List<Integer>> buyLotto,
+        List<Integer> prizeNumber,
+        int bonusNumber) {
+        for (List<Integer> eachLotto : buyLotto) {
+            checkCorrectLottoNumber(lottoPrize, eachLotto, prizeNumber, bonusNumber);
         }
+    }
 
-        for (Integer number : select_numbers) {
-            if (number > 45 || number < 1) {
-                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-                throw new IllegalArgumentException();
+    private boolean correctFiveAndBonus(int correctNumberCount, int bonusNumber,
+        List<Integer> eachLotto) {
+        return correctNumberCount == 5 && eachLotto.contains(bonusNumber);
+    }
+
+    public void checkCorrectLottoNumber(int[] lottoPrize, List<Integer> eachLotto,
+        List<Integer> prizeNumber,
+        int bonusNumber) {
+        int correctNumberCount = 0;
+        for (Integer lottoNumber : eachLotto) { // 스트림
+            if (prizeNumber.contains(lottoNumber)) {
+                correctNumberCount++;
             }
         }
-    }
 
-    private void getMoney() {
-        System.out.println("구입 금앱을 입력해 주세요.");
-        money = Integer.parseInt(Console.readLine());
-
-        if (money % 1000 != 0) {
-            System.out.println("[ERROR] 로또 금액을 잘못 입력하였습니다..");
-            throw new IllegalArgumentException();
-        }
-        System.out.printf("\n%d개를 구매했습니다.\n", money / 1000);
-    }
-
-    private void viewNumbers() {
-        for (List<Integer> number : numbers) {
-            System.out.println(number);
-        }
-    }
-
-    private List<Integer> getNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    }
-
-    private void getSelectNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        select_numbers = Stream.of(Console.readLine().split(",")).map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        select_numbers.add(Integer.parseInt(Console.readLine()));
-        validate();
-    }
-
-    private void Extract() {
-        for (List<Integer> number : numbers) {
-            checkLottoNumbers(number);
-        }
-    }
-
-    private void checkLottoNumbers(List<Integer> number) {
-        int correct_lotto_numbers = 0;
-        for (int i = 0; i < number.size(); i++) {
-            if (select_numbers.get(i).equals(number.get(i))) {
-                correct_lotto_numbers++;
-            }
-        }
-        if (correct_lotto_numbers==5 && number.contains(select_numbers.get(6))) {
-            ranking[0]++; // 2등.
+        if (correctFiveAndBonus(correctNumberCount, bonusNumber, eachLotto)) {
+            lottoPrize[getPrizeIndex(Prize.FIVE_AND_BONUS)]++;
             return;
         }
-        ranking[7-correct_lotto_numbers]++;
-    }
-
-    private void lottoResult () {
-        System.out.println("당첨 통계\n---");
-
-        System.out.printf("3개 일치 (5,000원) - %d개\n" , ranking[4]);
-        System.out.printf("4개 일치 (50,000원) - %d개\n" , ranking[3]);
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n" , ranking[2]);
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n" , ranking[0]);
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n" , ranking[1]);
-        System.out.printf("총 수익률은 %.1f",
-                (float)(ranking[4]*5000+ranking[3]*50000+ranking[2]*1500000+ranking[0]*30000000+ranking[1]*2000000000)/money);
-        System.out.print("%입니다.");
-    }
-
-    void Play() {
-        getMoney(); // 구입 금액 입력
-        setNumbers(); // 로또 번호 랜덤 추첨
-        viewNumbers(); // 로또 구매 결과 출력
-        getSelectNumbers(); // 당첨 , 보너스 번호 입력
-        Extract(); // 로또 번호 추첨
-        lottoResult(); // 결과 출력
+        lottoPrize[correctNumberCount]++;
     }
 }
 
+class InputView {
+
+    int money;
+    int bonusNumber;
+
+    public void buyLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        money = Integer.parseInt(Console.readLine()); // 금액 입력
+        System.out.printf("%d개를 구매했습니다.\n", money / 1000);
+
+    }
+
+    public void inputPrizeNumbers(List<Integer> prizeNumber) {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        prizeNumber.addAll(
+            Arrays.stream(Console.readLine().split(",")).map(Integer::parseInt).collect(
+                Collectors.toList()));
+    }
+
+    public void inputLottoBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        bonusNumber = (Integer.parseInt(Console.readLine()));
+    }
+}
+
+class OutputView {
+
+    public void viewBuyLotto(List<List<Integer>> buyLotto) {
+        for (List<Integer> lotto : buyLotto) {
+            System.out.println(lotto);
+        }
+    }
+
+
+    public void viewLottoResult(int[] lottoPrize, float profitRate, int money) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoPrize[Prize.THREE.ordinal()]);
+        System.out.printf("4개 일치 (50,000원) - %d개\n", lottoPrize[Prize.FOUR.ordinal()]);
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", lottoPrize[Prize.FIVE.ordinal()]);
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n",
+            lottoPrize[Prize.FIVE_AND_BONUS.ordinal()]);
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", lottoPrize[Prize.SIX.ordinal()]);
+        System.out.printf("총 수익률은 %.3f", profitRate / money);
+        System.out.print("%입니다.");
+
+    }
+}
