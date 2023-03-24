fadd81700bd4f90fb9076916c56f04b1f0615d47
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-24 13:18:23 +0900

    23-03-24 Commit

--- src/main/java/com/knight/homework/람다식.java
+++ src/main/java/com/knight/homework/람다식.java
@@ -1,8 +1,5 @@
 package com.knight.homework;
 
-import java.util.HashSet;
-import java.util.Set;
-
 interface Result {
 
   public abstract int plus1(int x, int y);
@@ -14,19 +11,13 @@ public class 람다식 {
 
   public static void main(String[] args) {
 
-    Set<Integer> set = new HashSet<>();
-    set.add(3);
-    set.add(2);
-    set.add(1);
-    set.add(4);
-    set.add(5);
+   Result result = (x,y) -> {
+     return x>y ? x:y;
+   };
 
-    // 저장된 데이터 출력
-    for (int num : set) {
-      System.out.println(num);
-    }
+    System.out.println(result.plus1(10,11));
 
-    System.out.println(set);
+
   }
 
 
