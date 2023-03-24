8a2772d0348305f8dfdd511806e783b4cea50f02
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-24 18:17:26 +0900

    23-03-24 Commit

---
+++ src/main/java/com/knight/homework/Test.java
@@ -0,0 +1,17 @@
+package com.knight.homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+  public static void main(String[] args) {
+
+    List<Integer> test = new ArrayList<>(Arrays.asList(1,2,3,4));
+
+    System.out.println(test.get(0));
+
+  }
+
+}
--- src/main/java/com/knight/homework/재귀함수.java
+++ src/main/java/com/knight/homework/재귀함수.java
@@ -1,18 +1,47 @@
 package com.knight.homework;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class 재귀함수 {
 
   static int result = 1;
+  static int sum = 0;
+  static int pibo = 0;
 
   public static int numSquare(int num, int square) {
 
     if (square == 0) {
       return 1;
     }
-//    } else if (square != 0) {
 
-//      result *= num;
     return result = num * numSquare(num, square - 1);
+
+
+  }
+
+
+  public static int numSum(int num) {
+
+    if (num == 0) {
+      return 0;
+    }
+    return sum += num + numSum(num - 1);
+
+  }
+
+  public static int numPibo(int fir, int se, int num) {
+
+    List<Integer> pibo = new ArrayList<>(Arrays.asList(fir, se));
+    for (int i = 0; i < num - 2; i++) {
+      int sum = 0;
+      int num1 = pibo.get(i);
+      int num2 = pibo.get(i + 1);
+      sum = num1 + num2;
+      pibo.add(sum);
+    }
+    return pibo.get(num - 1);
 
 
   }
@@ -21,6 +50,9 @@ public class 재귀함수 {
   public static void main(String[] args) {
 
     System.out.println(numSquare(3, 3));
+    System.out.println(numSum(5));
+    System.out.println(numPibo(1, 1, 7));
+
 
   }
 
