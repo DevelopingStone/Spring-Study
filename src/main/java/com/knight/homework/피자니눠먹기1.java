da4f7cb95d266c4e13f66ca947211029526799f5
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-29 19:53:33 +0900

    23-03-29 Commit

---
+++ src/main/java/com/knight/homework/피자니눠먹기1.java
@@ -0,0 +1,20 @@
+package com.knight.homework;
+
+public class 피자니눠먹기1 {
+
+  public static void main(String[] args) {
+
+  }
+}
+
+class Main2{
+
+  public int solution(int n) {
+
+    return (n + 6) / 7;
+
+
+
+  }
+
+    }
