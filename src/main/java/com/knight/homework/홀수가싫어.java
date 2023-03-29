37349929f4b6028f0779ba37552885bb920b1662
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-29 17:49:37 +0900

    23-03-29 Commit

---
+++ src/main/java/com/knight/homework/홀수가싫어.java
@@ -0,0 +1,30 @@
+package com.knight.homework;
+
+public class 홀수가싫어 {
+
+  public static void main(String[] args) {
+
+    Main1 user = new Main();
+    user.solution(10);
+
+
+  }
+
+}
+
+class Main1 {
+
+
+  public int[] solution(int n) {
+
+    for (int i = 1; i <= n; i += 2) {
+
+
+
+    }
+
+    return null;
+  }
+
+
+}
