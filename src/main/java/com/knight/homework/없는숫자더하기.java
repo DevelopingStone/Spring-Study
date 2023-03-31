ec8bd38f8d61c5d5b456dc8fd157b8cad5c53af1
kangrack333@gmail.com <kangrack333@gmail.com>
2023-03-31 14:17:21 +0900

    23-03-31 Commit

--- src/main/java/com/knight/homework/없는숫자더하기.java
+++ src/main/java/com/knight/homework/없는숫자더하기.java
@@ -1,10 +1,14 @@
 package com.knight.homework;
 
+import java.util.*;
+
 public class 없는숫자더하기 {
 
   public static void main(String[] args) {
 
     main7 user = new main7();
+    List<Integer> list = new ArrayList<>(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5));
+
 
   }
 
