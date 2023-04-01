70f6ff4e6bc4a6c31bf534157d531c125d49efba
kangrack333@gmail.com <kangrack333@gmail.com>
2023-04-01 14:14:47 +0900

    23-04-01 Commit

---
+++ src/main/java/com/knight/homework/문자열뒤집기.java
@@ -0,0 +1,21 @@
+package com.knight.homework;
+
+public class 문자열뒤집기 {
+
+  public static void main(String[] args) {
+   StringBuilder str = new StringBuilder("해랑사회민송");
+
+   str.reverse().toString();
+   str.append("락이가");
+    System.out.println("str = " + str);
+
+
+
+
+  }
+
+}
+
+class Main7{
+
+}
