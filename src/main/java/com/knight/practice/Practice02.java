87bcf15362ab777f4775ea45c2c84bccdc27a4c6
kangrack333@gmail.com <kangrack333@gmail.com>
2023-04-06 12:17:08 +0900

    23-04-06 Commit

--- src/main/java/com/knight/practice/Practice02.java
+++ src/main/java/com/knight/practice/Practice02.java
@@ -10,29 +10,33 @@ public class Practice02 {
 
     Main02 user = new Main02();
 //    System.out.println(user.solution("ab","adbak"));
-    user.solution("ab", "adbak");
+    System.out.println(user.solution("ab", "adbak"));
 
 
   }
-
 }
 
 class Main02 {
 
   public boolean solution(String str1, String str2) {
     List<String> list = new ArrayList<>(Arrays.asList(str2));
-    System.out.println(list);
+    boolean containsAB = false;
+    boolean containsBA = false;
 
-    for (String i : list) {
-      for (int j = 0; j*str1.length() < i.length(); j+=str1.length()) {
-        System.out.println(i.substring(j,j+str1.length()));
-
+    for (String str : list) {
+      for (int j = 0; j * str1.length() < str.length(); j += str1.length()) {
+        String substring = str.substring(j, j + str1.length());
+        if (substring.equals(str1)) {
+          containsAB = true;
+        } else if (substring.equals(new StringBuilder(str1).reverse().toString())) {
+          containsBA = true;
+        }
+        if (containsAB && containsBA) {
+          return true;
+        }
       }
-
     }
 
-    return true;
-
+    return false;
   }
-
 }
---
+++ src/main/java/com/knight/practice/Practice04.java
@@ -0,0 +1,66 @@
+package com.knight.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Practice04 {
+
+  public static void main(String[] args) {
+
+    Main05 user = new Main05();
+    user.solution(19);
+
+
+  }
+
+}
+
+
+class Main05 {
+
+  public boolean solution(int a) {
+    String str = Integer.toString(a);
+    List<Integer> list = new ArrayList<>();
+    List<Integer> all = new ArrayList<>();
+
+    for (int i = 0; i < str.length(); i++) {
+      list.add((int) str.charAt(i) - '0');
+
+    }
+    System.out.printf("입력값은 : %d , 쪼갠값은 : %s %n", a, list);
+
+    int sum = 0;
+    while (sum != 1) {
+      sum = 0;
+
+      for (int i = 0; i < list.size(); i++) {
+        sum += list.get(i) * list.get(i);
+
+      }
+      if (all.contains(sum)) {
+        System.out.println("sum 값 : " + sum);
+        System.out.printf("%d 는 무한반복되므로 행복한수가 아닙니다.", a);
+        break;
+      }
+      all.add(sum);
+      list.add(sum);
+      System.out.println("sum 값 : " + sum);
+      list = new ArrayList<>();
+      str = Integer.toString(sum);
+      for (int i = 0; i < str.length(); i++) {
+        list.add((int) str.charAt(i) - '0');
+      }
+      if (sum == 1) {
+        System.out.printf("%d는 1로 끝나는 수 이기 때문에 행복한수 입니다.%n", a);
+        return true;
+      }
+
+
+    }
+
+    return true;
+  }
+
+
+}
+
