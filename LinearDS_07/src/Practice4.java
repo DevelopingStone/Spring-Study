1a4684cd99c70ed32fd15c1d168268419c9cbe34
kangrack333@gmail.com <kangrack333@gmail.com>
2023-04-07 20:21:16 +0900

    23-04-07 Commit

--- LinearDS_07/src/Practice4.java
+++ LinearDS_07/src/Practice4.java
@@ -17,21 +17,48 @@ import java.util.Stack;
 
 public class Practice4 {
 
-//    public static boolean stringCompare(String s1, String s2) {
-//    }
+  public static boolean stringCompare(String s1, String s2) {
+    Stack stack1 = new Stack();
+    Stack stack2 = new Stack();
+    for (String i : s1.split("")) {
+      if (!i.equals("#")) {
+        stack1.push(i);
+      } else {
+        stack1.pop();
+      }
 
-    public static void main(String[] args) {
-        // Test code
-//        String s1 = "tree";
-//        String s2 = "th#ree";
-//        System.out.println(stringCompare(s1, s2));
-//
-//        s1 = "ab#a";
-//        s2 = "aab#";
-//        System.out.println(stringCompare(s1, s2));
-//
-//        s1 = "wo#w";
-//        s2 = "ww#o";
-//        System.out.println(stringCompare(s1, s2));
+
     }
+    for (String i : s2.split("")) {
+      if (!i.equals("#")) {
+        stack2.push(i);
+      } else {
+        stack2.pop();
+      }
+
+
+    }
+
+    if (stack1.equals(stack2)) {
+      return true;
+    } else {
+
+      return false;
+    }
+  }
+
+  public static void main(String[] args) {
+    // Test code
+    String s1 = "tree";
+    String s2 = "th#ree";
+    System.out.println(stringCompare(s1, s2));
+
+    s1 = "ab#a";
+    s2 = "aab#";
+    System.out.println(stringCompare(s1, s2));
+
+    s1 = "wo#w";
+    s2 = "ww#o";
+    System.out.println(stringCompare(s1, s2));
+  }
 }
