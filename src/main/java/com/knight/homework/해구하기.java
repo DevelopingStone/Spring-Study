f3b90178961dbb4e04a49d0624879f5b321299bd
kangrack333@gmail.com <kangrack333@gmail.com>
2023-04-08 18:10:28 +0900

    23-04-08 Commit

---
+++ src/main/java/com/knight/homework/해구하기.java
@@ -0,0 +1,33 @@
+package com.knight.homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 해구하기 {
+
+  public static void main(String[] args) {
+
+    List<Integer> num = new ArrayList<>();
+    List<String> str = new ArrayList<>();
+
+
+    String a = "x+5-3+x=6+x-2";
+    str.addAll(Arrays.asList(a.split("\\+|\\-")));
+    System.out.println(str);
+
+
+  }
+
+}
+
+class main08{
+  public String solution(){
+
+
+
+
+
+    return null;
+  }
+}
