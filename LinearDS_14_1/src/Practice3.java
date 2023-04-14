0c4e0b3af7241a3305cae608a1cee083a8e9b855
kangrack333@gmail.com <kangrack333@gmail.com>
2023-04-14 17:58:21 +0900

    23-04-14 Commit

--- LinearDS_14_1/src/Practice3.java
+++ LinearDS_14_1/src/Practice3.java
@@ -1,17 +1,29 @@
+import java.util.LinkedHashMap;
 
 public class Practice3 {
-    public static void solution(int[] data) {
+
+  public static void solution(int[] data) {
+
+  }
+
+  public static void main(String[] args) {
+
+//        int[] balloon = {3, 2, 1, -3, -1};
+//        solution(balloon);
+//
+//        System.out.println();
+//        balloon = new int[]{3, 2, -1, -2};
+//        solution(balloon);
+
+    LinkedHashMap<Integer, Integer> hm = new LinkedHashMap<>();
+
+    hm.put(100, 200);
+    hm.put(200, 300);
+    int firstKey = hm.keySet().iterator().next();
+    System.out.println("firstKey = " + firstKey);
+
+    System.out.print("바보");
 
 
-
-    }
-
-    public static void main(String[] args) {
-        int[] balloon = {3, 2, 1, -3, -1};
-        solution(balloon);
-
-        System.out.println();
-        balloon = new int[]{3, 2, -1, -2};
-        solution(balloon);
-    }
+  }
 }
