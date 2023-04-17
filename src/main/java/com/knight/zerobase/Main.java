61f8ef0658b86bd15005289f1f47bdbca2b2cb73
kangrack333@gmail.com <kangrack333@gmail.com>
2023-04-17 13:22:39 +0900

    23-04-17 Commit

--- src/main/java/com/knight/zerobase/Main.java
+++ src/main/java/com/knight/zerobase/Main.java
@@ -1,16 +1,36 @@
 package com.knight.zerobase;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-    Random random = new Random();
-    int num = random.nextInt(4);
-    System.out.println("num = " + num);
-    
 
+    List<Integer> list = new ArrayList<>();
+    Scanner scan = new Scanner(System.in);
+    int result = scan.nextInt();
+
+    int count = 0;
+    while (result > 0) {
+      int num = scan.nextInt();
+      list.add(num);
+
+      result--;
+    }
+
+    int replace = scan.nextInt();
+
+    for(Integer num : list){
+      if(replace==num){
+      count++;
+      }
+    }
+
+    System.out.println(count);
 
   }
+
 
 }
