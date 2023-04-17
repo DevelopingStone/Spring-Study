7046203d9cbd6e702466c32d87c7463466dc0093
kangrack333@gmail.com <kangrack333@gmail.com>
2023-04-18 00:58:52 +0900

    23-04-18 Commit

--- src/main/java/com/knight/homework/모스부호.java
+++ src/main/java/com/knight/homework/모스부호.java
@@ -11,6 +11,7 @@ public class 모스부호 {
     Solution02 user = new Solution02();
     System.out.println(user.solution(s));
 
+
   }
 
 }
@@ -19,8 +20,10 @@ public class 모스부호 {
 class Solution02 {
 
   public String solution(String letter) {
-    String str = "";
+
     Map<String, String> map = new HashMap<>();
+    String str = "";
+
     map.put(".-", "a");
     map.put("-...", "b");
     map.put("-.-.", "c");
@@ -48,15 +51,11 @@ class Solution02 {
     map.put("-.--", "y");
     map.put("--..", "z");
 
-//    for (String key : map.keySet()) {
-//      letter = letter.replace(key, map.get(key).toString());
-//
-//    }
-//
-//    return letter;
     for (String morse : letter.split(" ")) {
-      String decodedChar = map.get(morse);
-      str += decodedChar == null ? "" : decodedChar;
+
+      String result = map.get(morse);
+      str += result;
+
     }
 
     return str;
