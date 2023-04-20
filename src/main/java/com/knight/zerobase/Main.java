f0e07568baa4e18f8fbec74ce3f67dff2598b43e
kangrack333@gmail.com <kangrack333@gmail.com>
2023-04-20 13:36:30 +0900

    23-04-20 Commit

--- src/main/java/com/knight/zerobase/Main.java
+++ src/main/java/com/knight/zerobase/Main.java
@@ -1,9 +1,5 @@
 package com.knight.zerobase;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
 
   public static void main(String[] args) {
@@ -11,14 +7,9 @@ public class Main {
     String[] bj = {"혁준", "하밥", "양상", "심심이", "소스왕"};
     String[] one = {"혁준", "양상"};
     String[] two = {"심심이", "소스왕"};
-    int[] arr = {3, 76, 24};
-    String s = "Hello, World!?";
 
-    int[] A = {6, 12, 4};
-    int[] orders = {2, 4, 5, 7};
-    int[] array1 = {31, 21, 5, 12, 1};
-    int[] emergency = {3, 76, 24};
-    System.out.println(user.solution(emergency));
+    String[] arr = {"10110", "1010", "11110"};
+    System.out.println(user.solution(bj));
 
 
   }
@@ -28,27 +19,12 @@ public class Main {
 
 class Solution {
 
-  public int[] solution(int[] emergency) {
-    List<Integer> list = new ArrayList<>();
-    List<Integer> result = new ArrayList<>();
-    for (int num : emergency) {
-      list.add(num);
-      result.add(num);
-
+  public String solution(String[] arr) {
+    String str = "";
+    for (String result : arr) {
+      str += result;
+      str += ",";
     }
-
-    Collections.sort(list, Collections.reverseOrder());
-    System.out.println(result);
-    System.out.println(list);
-
-    for (int i = 0; i < list.size(); i++) {
-      for (int j = 0; j < list.size(); j++) {
-        if (result.get(i).equals(list.get(j))) {
-          emergency[i] = j + 1;
-        }
-      }
-    }
-
-    return emergency;
+    return str = str.substring(0, str.length() - 1);
   }
 }
