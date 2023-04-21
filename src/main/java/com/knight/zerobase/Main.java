da96df32fa71160083c3a59309d13c450a994f60
kangrack333@gmail.com <>
2023-04-22 04:10:03 +0900

    23-04-21 Commit

--- src/main/java/com/knight/zerobase/Main.java
+++ src/main/java/com/knight/zerobase/Main.java
@@ -1,8 +1,16 @@
 package com.knight.zerobase;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+
 public class Main {
 
   public static void main(String[] args) {
+
+    String[] name = {"mumu", "soe", "poe", "kai", "mine"};
+    String[] call = {"kai", "kai", "mine", "mine"};
+    Solution user = new Solution();
+    System.out.println(user.solution(name, call));
 
   }
 
@@ -11,17 +19,38 @@ public class Main {
 
 class Solution {
 
-  public int solution(int n) {
+  public String[] solution(String[] players, String[] callings) {
+    LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+    String call = "";
+    for (int i = 0; i < players.length; i++) {
+      map.put(players[i], i);
+    }
+    System.out.println(map);
 
+    for (int i = 0; i < callings.length; i++) {
+      int num = map.get(callings[i]);
 
+      call = players[num - 1];
+      players[num - 1] = players[num];
+      players[num] = call;
+      map.put(players[num], num);
+      map.put(players[num - 1], num - 1);
 
+    }
 
+    System.out.println(Arrays.toString(players));
 
+    return players;
 
-    return 0;
 
   }
 }
+
+
+
+
+
+
 
 
 
