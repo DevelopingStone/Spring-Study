222644a106f2b8864af602eb43b0be8e1e805528
kangrack333@gmail.com <>
2023-04-21 22:06:37 +0900

    23-04-21 Commit

--- src/main/java/com/knight/zerobase/Main.java
+++ src/main/java/com/knight/zerobase/Main.java
@@ -3,14 +3,6 @@ package com.knight.zerobase;
 public class Main {
 
   public static void main(String[] args) {
-    Solution user = new Solution();
-    String[] bj = {"혁준", "하밥", "양상", "심심이", "소스왕"};
-    String[] one = {"혁준", "양상"};
-    String[] two = {"심심이", "소스왕"};
-
-    String[] arr = {"10110", "1010", "11110"};
-    System.out.println(user.solution(bj));
-
 
   }
 
@@ -19,12 +11,15 @@ public class Main {
 
 class Solution {
 
-  public String solution(String[] arr) {
-    String str = "";
-    for (String result : arr) {
-      str += result;
-      str += ",";
-    }
-    return str = str.substring(0, str.length() - 1);
+  public int solution(int n) {
+
+    return 0;
+
   }
 }
+
+
+
+
+
+
