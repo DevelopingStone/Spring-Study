ed026e80f62fb1e842e636627c13b1e72018e5cd
박강락 <kangrack333@gmail.com>
2023-04-22 12:22:10 +0900

    23-04-21 Commit

--- src/main/java/com/knight/zerobase/Main.java
+++ src/main/java/com/knight/zerobase/Main.java
@@ -30,7 +30,8 @@ class Solution {
     for (int i = 0; i < callings.length; i++) {
       int num = map.get(callings[i]);
 
-      
+
+
       call = players[num - 1];
       players[num - 1] = players[num];
       players[num] = call;
