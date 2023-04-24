1bdb397b27975da482def138048a1440b69c5b50
박강락 <kangrack333@gmail.com>
2023-04-24 10:40:32 +0900

    23-04-24 Commit

---
+++ src/main/java/com/knight/homework/문자열안에문자열.java
@@ -0,0 +1,60 @@
+package com.knight.homework;
+
+import java.util.Scanner;
+
+public class 문자열안에문자열 {
+
+  public static void main(String[] args) {
+
+//    Solution22 user = new Solution22();
+//    System.out.println(user.solution("ab6CDE443fgh22iJKlmn1o", "6CD"));
+
+    Scanner scan = new Scanner(System.in);
+    String str = "";
+    StringBuilder sb = new StringBuilder();
+    int sum = 0;
+
+    while (!"=".equals(str)) {
+      str = scan.next();
+      if("+".equals(str)){
+        str = scan.next();
+        sum+=Integer.parseInt(str);
+      }
+      else if("-".equals(str)){
+        str = scan.next();
+        sum-=Integer.parseInt(str);
+      }
+      else if("*".equals(str)){
+        str = scan.next();
+        sum*=Integer.parseInt(str);
+      }
+      else if("/".equals(str)){
+        str = scan.next();
+        sum/=Integer.parseInt(str);
+      }
+      else if("=".equals(str)){
+        break;
+      }
+      else{
+        sum+=Integer.parseInt(str);
+      }
+
+
+    }
+    System.out.println(sum);
+
+
+  }
+
+}
+
+
+class Solution22 {
+
+  public int solution(String str1, String str2) {
+
+    System.out.println(str1.contains(str2));
+
+    return 2;
+  }
+}
