1c6f4f3facea813f42aeff714ec0dc4da771b5fa
박강락 <kangrack333@gmail.com>
2023-04-30 20:47:50 +0900

    23-04-30 Commit

--- NonLinearDS_01/src/Practice1.java
+++ NonLinearDS_01/src/Practice1.java
@@ -6,24 +6,70 @@ class BinaryTree {
   char[] arr;
 
   BinaryTree(char[] data) {
-    arr = data.clone();
+    this.arr = data.clone();
   }
 
   public void preOrder(int idx) {
     System.out.print(this.arr[idx] + " ");
+
     int left = 2 * idx + 1;
-
     int right = 2 * idx + 2;
-
     if (left < this.arr.length) {
-
+//      System.out.println("left = " + left);
       this.preOrder(left);
-    }
-    if (right < this.arr.length) {
 
+    }
+
+    if (right < this.arr.length) {
+//      System.out.println("right = " + right);
       this.preOrder(right);
+
     }
   }
+
+  public void inOrder(int idx) {
+
+    int left = idx * 2 + 1;
+    int right = idx * 2 + 2;
+    if (left < this.arr.length) {
+
+      inOrder(left);
+    }
+    System.out.print(this.arr[idx] + " ");
+
+    if (right < this.arr.length) {
+
+      inOrder(right);
+    }
+
+
+  }
+
+  public void postOrder(int idx) {
+
+    int left = idx * 2 + 1;
+    int right = idx * 2 + 2;
+    if (left < this.arr.length) {
+
+      postOrder(left);
+    }
+
+    if (right < this.arr.length) {
+
+      postOrder(right);
+    }
+    System.out.print(this.arr[idx] + " ");
+
+  }
+
+  public void levelOrder(int idx) {
+
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(this.arr[i] + " ");
+    }
+    System.out.println();
+  }
+
 
 }
 
@@ -41,17 +87,17 @@ public class Practice1 {
     System.out.println("== Preorder ==");
     bt.preOrder(0);
     System.out.println();
+
+    System.out.println("== Inorder ==");
+    bt.inOrder(0);
+    System.out.println();
 //
-//        System.out.println("== Inorder ==");
-//        bt.inOrder(0);
-//        System.out.println();
-//
-//        System.out.println("== Postorder ==");
-//        bt.postOrder(0);
-//        System.out.println();
-//
-//        System.out.println("== Levelorder ==");
-//        bt.levelOrder(0);
-//        System.out.println();
+    System.out.println("== Postorder ==");
+    bt.postOrder(0);
+    System.out.println();
+
+    System.out.println("== Levelorder ==");
+    bt.levelOrder(0);
+    System.out.println();
   }
 }
