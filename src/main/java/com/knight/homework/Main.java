26e9a0b0568df06d6ab4385ce2acb8769e79a6f1
박강락 <kangrack333@gmail.com>
2023-04-30 12:09:45 +0900

    23-04-30 Commit

--- src/main/java/com/knight/homework/Main.java
+++ src/main/java/com/knight/homework/Main.java
@@ -3,42 +3,15 @@ package com.knight.homework;
 
 public class Main {
 
-  // 필드값, 생성자, 메소드, 메인메소드
- //  생성자를 만들떄는 기본생성자를 꼭 만들어주자
-//  private
-
-  //필드값
-  private String name;
-//  박강락
-  private int age;
-//  20
-
-  //생성자
-  Main(){
-
-  }
-
-  Main(String name, int age){
-    this.name = name;
-    this.age = age;
-  }
-
-
-
-
-  //메소드
-  void printInfo(){
-    System.out.println("name = " + this.name + ", Age : " + this.age);
-  }
-
   public static void main(String[] args) {
-    Main user = new Main("박강락",20);
 
-
-    user.printInfo();
-
-
+    int x = 10;
+    int y = 20;
+    int z = (++x) + (y--);
+    System.out.println(x);
+    System.out.println(y);
   }
+
 
 
 }
--- src/main/java/com/knight/zerobase/practice/two/ThreeAnd0303.java
+++ src/main/java/com/knight/zerobase/practice/two/ThreeAnd0303.java
@@ -7,7 +7,8 @@ public class ThreeAnd0303 {
   public static void main(String[] args) {
     String S = "2*3+5/6*3+15";
     Solution0303 user = new Solution0303();
-    System.out.println(user.evaluateExpression(S));
+    double result = user.evaluateExpression(S);
+    System.out.printf("%.2f",result);
 
   }
 
@@ -19,41 +20,41 @@ class Solution0303 {
   public double evaluateExpression(String expression) {
     expression = expression.replaceAll("\\s+", ""); // 공백 제거
 
-    double currentNumber = 0;
-    char currentOperator = '+';
-    Stack<Double> stack = new Stack<>();
+    double currentNumber = 0; // 현재 처리 중인 숫자
+    char currentOperator = '+'; // 현재 처리 중인 연산자, 초기값은 덧셈
+    Stack<Double> stack = new Stack<>(); // 스택 생성
 
     for (int i = 0; i < expression.length(); i++) {
-      char c = expression.charAt(i);
+      char c = expression.charAt(i); // 문자열에서 i번째 문자 추출
 
-      if (Character.isDigit(c)) {
-        currentNumber = currentNumber * 10 + (c - '0');
+      if (Character.isDigit(c)) { // 숫자인 경우
+        currentNumber = currentNumber * 10 + (c - '0'); // 숫자 계산
       }
 
+      // 연산자인 경우 또는 문자열의 마지막 문자인 경우
       if (!Character.isDigit(c) || i == expression.length() - 1) {
-        if (currentOperator == '+') {
-          stack.push(currentNumber);
-        } else if (currentOperator == '-') {
-          stack.push(-currentNumber);
-        } else if (currentOperator == '*') {
-          double top = stack.pop();
-          stack.push(top * currentNumber);
-        } else if (currentOperator == '/') {
-          double top = stack.pop();
-          stack.push(top / currentNumber);
+        if (currentOperator == '+') { // 덧셈인 경우
+          stack.push(currentNumber); // 스택에 현재 숫자 추가
+        } else if (currentOperator == '-') { // 뺄셈인 경우
+          stack.push(-currentNumber); // 스택에 음수 형태로 현재 숫자 추가
+        } else if (currentOperator == '*') { // 곱셈인 경우
+          double top = stack.pop(); // 스택에서 최상위 값 제거
+          stack.push(top * currentNumber); // 제거한 값과 현재 숫자를 곱한 결과를 스택에 추가
+        } else if (currentOperator == '/') { // 나눗셈인 경우
+          double top = stack.pop(); // 스택에서 최상위 값 제거
+          stack.push(top / currentNumber); // 제거한 값과 현재 숫자를 나눈 결과를 스택에 추가
         }
 
-        currentOperator = c;
-        currentNumber = 0;
+        currentOperator = c; // 현재 처리 중인 연산자를 갱신
+        currentNumber = 0; // 현재 처리 중인 숫자를 초기화
       }
     }
 
-    double result = 0;
-    while (!stack.isEmpty()) {
-      result += stack.pop();
+    double result = 0; // 최종 계산 결과
+    while (!stack.isEmpty()) { // 스택이 비어있지 않을 때까지 반복
+      result += stack.pop(); // 스택에서 값을 꺼내서 최종 결과에 더함
     }
 
-    return result;
+    return result; // 최종 결과 반환
   }
 }
-
