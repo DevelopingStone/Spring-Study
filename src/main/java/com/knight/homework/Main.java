e90780528ba328f6324ac7f89ebfad36c2c2a0ce
박강락 <kangrack333@gmail.com>
2023-05-04 01:51:56 +0900

    23-05-04 Commit

--- src/main/java/com/knight/homework/Main.java
+++ src/main/java/com/knight/homework/Main.java
@@ -2,16 +2,19 @@ package com.knight.homework;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
 
   public static void main(String[] args) {
     Solution user = new Solution();
-    int[] arr = {100, 95, 95, 95, 90, 90, 50, 40, 30, 20, 10, 15, 70, 55, 90, 80};
-
-    System.out.println(user.solution(232443, 4));
+    int[] num = {7, 77, 17};
+    String str = "abc1Addfggg4556b";
+    System.out.println(user.solution(5278));
+    System.out.println(Math.sqrt(144));
 
 
   }
@@ -21,8 +24,26 @@ public class Main {
 
 
 class Solution {
-  public int solution(int num, int k) {
-    System.out.println(String.valueOf(k));
-    return ("-" + num).indexOf(String.valueOf(k));
+
+  public Set<Integer> solution(int n) {
+    Set<Integer> factors = new LinkedHashSet<>();
+
+    // 2부터 n의 제곱근까지 모든 수에 대해 반복
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+      // i가 n의 소인수인 경우
+      while (n % i == 0) {
+        // i를 소인수 목록에 추가하고, n을 i로 나눔
+        factors.add(i);
+        n /= i;
+      }
+    }
+
+    // n이 소인수인 경우
+    if (n > 1) {
+      factors.add(n);
+    }
+
+
+    return factors;
   }
 }
