73498f8edfa5815a1611e28c0a36dd9079bc540c
박강락 <kangrack333@gmail.com>
2023-05-12 20:44:14 +0900

    23-05-11 Commit

--- src/main/java/com/knight/homework/Main.java
+++ src/main/java/com/knight/homework/Main.java
@@ -1,7 +1,9 @@
 package com.knight.homework;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -9,8 +11,14 @@ public class Main {
     Solution user = new Solution();
     int[] arr1 = {1, 2, 3, 4};
     int[] arr2 = {2, 4, 8};
+    int[][] arr = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 1, 0},
+        {0, 0, 0, 0, 0}};
 
-    System.out.println(user.solution(4, 14));
+    System.out.println(user.solution("x + 10 + 9x + 20x"));
+
+    String [] str = {"a","b","c"};
+    List<String> list = Arrays.asList(str.clone());
+
 
 
   }
@@ -20,15 +28,39 @@ public class Main {
 
 
 class Solution {
-  public int[] solution(int num, int total) {
-    int[] answer = new int[num];
-    int check = num*(num+1) / 2;
-    int start = (total - check) / num + 1;
-    for (int i = 0; i < answer.length; i++) {
-      answer[i] = start + i ;
-    }
+  public String solution(String polynomial) {
+    int numX = 0;
+    int num = 0;
+    for (String word : polynomial.split(" ")) {
 
-    System.out.println(Arrays.toString(answer));
-    return answer;
+
+      if(word.contains("x")){
+        for(int i =0; i<word.length(); i++){
+          if(word.charAt(i)!='x'){
+            System.out.println("word.charAt(i) = " + word.charAt(i));
+
+            numX+=Integer.parseInt(String.valueOf(word.charAt(i)));
+            numX--;
+          }
+          else if (word.charAt(i)=='x'){
+            numX++;
+          }
+
+        }
+
+
+
+
+      }
+      else if(word.matches("[0-9]+")){
+
+        num+=Integer.parseInt(word);
+      }
+    }
+    System.out.println(num);
+    System.out.println(numX);
+
+
+    return null;
   }
 }
