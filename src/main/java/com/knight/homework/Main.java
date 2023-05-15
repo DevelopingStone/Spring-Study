68a67773366e1ec4725845960f2e33161c5cdb39
박강락 <kangrack333@gmail.com>
2023-05-15 16:31:34 +0900

    23-05-15 Commit

--- src/main/java/com/knight/homework/Main.java
+++ src/main/java/com/knight/homework/Main.java
@@ -1,12 +1,8 @@
 package com.knight.homework;
 
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -23,88 +19,50 @@ public class Main {
 class Solution {
 
   public int solution(int[][] lines) {
-    Set<Integer> set1 = new HashSet<>();
-    Set<Integer> set2 = new HashSet<>();
-    Set<Integer> set3 = new HashSet<>();
-    Set<Integer> result = new LinkedHashSet<>();
-    Set<Integer> result1 = new LinkedHashSet<>();
-    Set<Integer> result2 = new LinkedHashSet<>();
-    for (int i = lines[0][0]; i <= lines[0][1]; i++) {
-      set1.add(i);
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < lines.length; i++) {
+      int min = Math.min(lines[i][0], lines[i][1]);
+      System.out.println("min = " + min);
+      int max = Math.max(lines[i][0], lines[i][1]);
+      System.out.println("max = " + max);
+
+      /*for (int j = min; j < max; j++) {
+
+        int value = map.getOrDefault(j, 0);
+        map.put(j, value + 1);
+
+
+      }*/
+
+      for(int j=min; j<max; j++){
+        int value = map.getOrDefault(j,0);
+        map.put(j,value+1);
+
+      }
+
+
 
 
     }
-    for (int i = lines[1][0]; i <= lines[1][1]; i++) {
-      set2.add(i);
+    System.out.println("map = " + map);
 
-
-    }
-
-    for (int i = lines[2][0]; i <= lines[2][1]; i++) {
-      set3.add(i);
-
-
-    }
-
-    System.out.println("set1 = " + set1);
-    System.out.println("set1 = " + set2);
-    System.out.println("set1 = " + set3);
-
-
-
-    for(int num : set1){
-      if(set2.contains(num)){
-        result.add(num);
+    int answer = 0;
+    for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+      if(entry.getValue()>=2){
+        answer++;
       }
     }
-//    if(result.size()==1){
-//      result.clear();
-//    }
-    for(int num : set1){
-      if(set3.contains(num)&&!result.contains(num)){
-        result1.add(num);
+
+    /*for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      if (entry.getKey() >= 2) {
+        System.out.println("entry.getValue() = " + entry.getValue());
+        answer++;
       }
-    }
-//    if(result1.size()==1){
-//      result1.clear();
-//    }
-    for(int num : set2){
-      if(set3.contains(num)&&!result.contains(num)&&!result1.contains(num)){
-        result2.add(num);
-      }
-    }
-//    if(result2.size()==1){
-//      result2.clear();
-//    }
-
-    int sum = 0;
-    System.out.println(result);
-    System.out.println(result1);
-    System.out.println(result2);
-    if(result.size()==0 || result.size()==1){
+    }*/
 
 
-    }
-    else{
-      sum+=result.size()-1;
-    }
 
-    if(result1.size()==0 || result1.size()==1){
-
-    }
-    else{
-      sum+=result1.size()-1;
-    }
-
-    if(result2.size()==0 || result2.size()==1){
-
-    }
-    else{
-      sum+=result2.size()-1;
-    }
-
-
-    return sum;
+    return answer;
   }
 }
-
