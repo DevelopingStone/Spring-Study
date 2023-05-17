2d34ce48ea4c29ab60b52be733dd91f0d1820674
박강락 <kangrack333@gmail.com>
2023-05-18 01:42:11 +0900

    23-05-17 Commit

--- src/main/java/com/knight/homework/Main.java
+++ src/main/java/com/knight/homework/Main.java
@@ -1,34 +1,108 @@
 package com.knight.homework;
 
 
-class Counter implements Runnable {
-  private int count = 0;
-
-  @Override
-  public void run() {
-    for (int i = 0; i < 1000000; i++) {
-      count++; // count 값을 1 증가
-    }
-  }
-
-  public int getCount() {
-    return count;
-  }
-}
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
-  public static void main(String[] args) throws InterruptedException {
-    Counter counter = new Counter();
 
-    Thread thread1 = new Thread(counter);
-    Thread thread2 = new Thread(counter);
+  public static void main(String[] args) {
+    Solution user = new Solution();
+//    int[][] array = {{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1},
+//        {1, 1, 1, 1, 1}};
+    int [] array = {7, 3, 2, 13, 9, 15, 8, 11};
+    System.out.println(user.solution(array,12));
+  }
 
-    thread1.start();
-    thread2.start();
 
-    thread1.join();
-    thread2.join();
+}
 
-    System.out.println("Count: " + counter.getCount());
+
+/*
+class Solution {
+
+  public int solution(int[][] lines) {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < lines.length; i++) {
+      int min = Math.min(lines[i][0], lines[i][1]);
+      System.out.println("min = " + min);
+      int max = Math.max(lines[i][0], lines[i][1]);
+      System.out.println("max = " + max);
+
+
+      for(int j=min; j<max; j++){
+        int value = map.getOrDefault(j,0);
+        map.put(j,value+1);
+
+      }
+
+
+
+
+    }
+    System.out.println("map = " + map);
+
+    int answer = 0;
+    for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+      if(entry.getValue()>=2){
+        answer++;
+      }
+    }
+
+
+
+
+    return answer;
+  }
+}*/
+
+/*class Solution {
+  int a = 0;
+  int b = 0;
+  public int solution(int[][] dots) {
+    System.out.println(dots.length);
+    System.out.println(dots[0].length);
+    for (int i = 0; i <dots.length ; i++) {
+      for (int j = i+1; j <dots[i].length ; j++) {
+        int a = dots[i][j] - dots[i][i];
+        int b = dots[j][j] - dots[i][j];
+
+
+
+      }
+    }
+
+
+
+    return 0;
+  }
+}*/
+
+
+class Solution {
+
+  public List<Integer> solution(int[] array,int target){
+    List<Integer> list = new LinkedList<>();
+    List<Integer> list1 = new LinkedList<>();
+    for(int num : array){
+      list.add(num);
+    }
+    Collections.sort(list);
+    for(int i =0; i<list.size(); i++){
+      for(int j=i+1; j<list.size();j++){
+        if(list.get(i)+list.get(j)==target){
+          list1.add(list.get(i));
+          list1.add(list.get(j));
+          return list1;
+
+        }
+      }
+    }
+
+
+
+    return null;
   }
 }
