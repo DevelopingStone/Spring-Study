a8689044f6c90c3d6ab092232ec4696848f5efd1
박강락 <kangrack333@gmail.com>
2023-05-24 10:34:04 +0900

    23-05-24 Commit

--- src/main/java/com/knight/homework/Main.java
+++ src/main/java/com/knight/homework/Main.java
@@ -1,8 +1,7 @@
 package com.knight.homework;
 
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Main {
@@ -11,8 +10,9 @@ public class Main {
     Solution user = new Solution();
 //    int[][] array = {{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1},
 //        {1, 1, 1, 1, 1}};
-    int[] array = {7, 3, 2, 13, 9, 15, 8, 11};
-    System.out.println(user.solution(5, 555));
+    int[][] array = {{1, 4}, {9, 2}, {3, 8}, {-5, 10}};
+    int[] value = {3,3,3,2,2,2};
+    System.out.println(user.solution(value));
 
 
   }
@@ -84,41 +84,18 @@ class Solution {
 
 
 class Solution {
-  public int[][] solution(int n) {
-    int[][] result = new int[n][n];
 
-    int value = 1; // 채워넣을 숫자
-    int rowStart = 0; // 행 시작 인덱스
-    int rowEnd = n - 1; // 행 끝 인덱스
-    int colStart = 0; // 열 시작 인덱스
-    int colEnd = n - 1; // 열 끝 인덱스
-
-    while (value <= n * n) {
-      // 왼쪽에서 오른쪽으로
-      for (int i = colStart; i <= colEnd; i++) {
-        result[rowStart][i] = value++;
-      }
-      rowStart++;
-
-      // 위쪽에서 아래쪽으로
-      for (int i = rowStart; i <= rowEnd; i++) {
-        result[i][colEnd] = value++;
-      }
-      colEnd--;
-
-      // 오른쪽에서 왼쪽으로
-      for (int i = colEnd; i >= colStart; i--) {
-        result[rowEnd][i] = value++;
-      }
-      rowEnd--;
-
-      // 아래쪽에서 위쪽으로
-      for (int i = rowEnd; i >= rowStart; i--) {
-        result[i][colStart] = value++;
-      }
-      colStart++;
+  public int solution(int[] nums) {
+    Map<Integer, Integer> map = new LinkedHashMap<>();
+    for (int i : nums) {
+      int value = map.getOrDefault(i, 0);
+      map.put(i, value + 1);
     }
 
-    return result;
+    if (map.size() < nums.length / 2) {
+      return map.size();
+    } else {
+      return nums.length / 2;
+    }
   }
 }
