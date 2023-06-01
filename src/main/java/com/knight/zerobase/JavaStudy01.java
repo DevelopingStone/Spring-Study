96de769f177b257af4e3af7781a4c1e2c62f9fc4
Knight <kangrack333@gmail.com>
2023-06-01 20:42:24 +0900

    2023-06-01

--- src/main/java/com/knight/zerobase/JavaStudy01.java
+++ src/main/java/com/knight/zerobase/JavaStudy01.java
@@ -5,19 +5,17 @@ package com.knight.zerobase;
  */
 public class JavaStudy01 {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 9; i++) {
-      for (int j = 1; j <= 9; j++) {
-        System.out.print(String.format("%02d x %02d = %02d \n", j, i, i * j));
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(String.format("%02d x %02d = %02d \n", j, i, i * j));
 
-      }
-      System.out.println();
+            }
+            System.out.println();
+        }
+
+
     }
-
-
-
-
-  }
 
 }
