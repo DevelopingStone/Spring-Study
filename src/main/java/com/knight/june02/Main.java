4f6c6a17caac13f9dcac0d5c7024a4ca9da03dad
Knight <kangrack333@gmail.com>
2023-06-04 13:04:40 +0900

    2023-06-04

--- src/main/java/com/knight/june02/Main.java
+++ src/main/java/com/knight/june02/Main.java
@@ -1,74 +1,34 @@
 package com.knight.june02;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
         Solution user = new Solution();
-        System.out.println(user.solution(6, 3, 3, 6));
-
-
+        String[] array = {"12", "123", "1235", "567", "88"};
+        System.out.println(user.solution(array));
     }
 }
 
-
 class Solution {
-    public int solution(int a, int b, int c, int d) {
-        int[] num = {a, b, c, d};
-        int result = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int numArray : num) {
-            int value = map.getOrDefault(numArray, 0);
-            map.put(numArray, value + 1);
+    public boolean solution(String[] phone_book) {
+        Map<String, Integer> map = new HashMap<>();
+        int i = 0;
+        for (String word : phone_book) {
+            map.put(word, i);
+            i++;
         }
+        System.out.println(map);
 
-        if (map.containsValue(4)) {
-            return 1111 * a;
-        } else if (map.containsValue(3) && map.containsValue(1)) {
-            for (Map.Entry<Integer, Integer> mapNum : map.entrySet()) {
-                if (mapNum.getValue() >= 3) {
-                    result += mapNum.getKey() * 10;
-                } else {
-                    result += mapNum.getKey();
+        for (int j = 0; j < phone_book.length; j++) {
+            for (int k = 1; k < phone_book[j].length(); k++) {
+                if (map.containsKey(phone_book[j].substring(0, k))) {
+                    return false;
                 }
             }
-            return result * result;
-
-
-        } else if (map.containsValue(2) && map.containsValue(1)) {
-            result = 1;
-            for (Map.Entry<Integer, Integer> mapNum : map.entrySet()) {
-                if (mapNum.getValue() == 1) {
-                    result *= mapNum.getKey();
-                }
-            }
-            return result;
-
-
-        } else if (map.containsValue(2)) {
-            int[] keys = new int[2];
-            int position = 0;
-            for (Map.Entry<Integer, Integer> mapNum : map.entrySet()) {
-
-                keys[position] = mapNum.getKey();
-                position++;
-            }
-            return (keys[0] + keys[1]) * Math.abs(keys[0] - keys[1]);
-
-
-        } else {
-            result = 7;
-            for (Map.Entry<Integer, Integer> mapNum : map.entrySet()) {
-                if (mapNum.getKey() < result) {
-                    result = mapNum.getKey();
-                }
-            }
-            return result;
         }
 
 
+        return true;
     }
 }
