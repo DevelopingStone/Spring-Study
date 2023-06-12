8b29616fa4e53a1c27ee9c06f49f1c12b23eca2c
Knight <kangrack333@gmail.com>
2023-06-13 02:49:34 +0900

    2023-06-10

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -1,38 +1,54 @@
 package com.knight.main;
 
-
-import java.sql.SQLOutput;
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+        String[] survey = {"TR", "RT", "TR"};
+        int[] choices = {7, 1, 3};
         Solution user = new Solution();
-        System.out.println(user.solution(plays));
+        System.out.println(user.solution(survey, choices));
 
     }
 }
 
-class Solution {
-    public int solution(int[] ingredient) {
-        int[] stack = new int[ingredient.length];
-        int sp = 0;
-        int answer = 0;
-        for (int i : ingredient) {
-            stack[sp++] = i;
 
-            System.out.println(Arrays.toString(stack));
-            if (sp >= 4 && stack[sp - 1] == 1
-                    && stack[sp - 2] == 3
-                    && stack[sp - 3] == 2
-                    && stack[sp - 4] == 1) {
-                sp -= 4;
-                answer++;
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        StringBuilder sb = new StringBuilder();
+        map.put('R', 0);map.put('T', 0);map.put('C', 0);map.put('F', 0);
+        map.put('J', 0);map.put('M', 0);map.put('A', 0);map.put('N', 0);
+
+        for (int i = 0; i < survey.length; i++) {
+            if (choices[i] > 4) map.put(survey[i].charAt(1), map.get(survey[i].charAt(1)) + choices[i] - 4);
+            else if (choices[i] < 4) {
+                map.put(survey[i].charAt(0), map.get(survey[i].charAt(0)) + 4 - choices[i]);
             }
         }
-        return answer;
+        if (map.get('R') < map.get('T')) {
+            sb.append('T');
+        } else {
+            sb.append('R');
+        }
+        if (map.get('C') < map.get('F')) {
+            sb.append('F');
+        } else {
+            sb.append('C');
+        }
+        if (map.get('J') < map.get('M')) {
+            sb.append('M');
+        } else {
+            sb.append('J');
+        }
+        if (map.get('A') < map.get('N')) {
+            sb.append('N');
+        } else {
+            sb.append('A');
+        }
+        return sb.toString();
     }
 }
