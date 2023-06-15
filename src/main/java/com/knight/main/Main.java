1edb349564597d2173a9542443c7cd67600f8dfa
Knight <kangrack333@gmail.com>
2023-06-15 19:38:37 +0900

    2023-06-15

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -1,62 +1,56 @@
 package com.knight.main;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        int[] progresses = {96, 94};
-        int[] speeds = {3, 3};
         Solution user = new Solution();
-        System.out.println(user.solution(progresses, speeds));
+
+//        String number = "1924";
+//        int k = 2;
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 3},
+                {0, 2, 5, 0, 1},
+                {4, 2, 4, 4, 2},
+                {3, 5, 1, 3, 1}
+        };
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+
+        System.out.println(user.solution(board, moves));
+//
+//        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+//        System.out.println(user.solution(clothes));
+
 
     }
 }
 
 
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
-        int[] pro = progresses.clone();
-        int day = 0;
+    public int solution(int[][] board, int[] moves) {
+        int[][] doll = board.clone();
+        Deque<Integer> deque = new LinkedList<>();
         int count = 0;
 
-        for (int i = 0; i < progresses.length; i++) {
-
-            if (i >= 1 && count >= 1 && pro[i] + (speeds[i] * day) < 100) {
-                list.add(count);
-                count = 0;
-            }
-
-            if (pro[i] + (speeds[i] * day) < 100) {
-                while (pro[i] < 100) {
-                    pro[i] += speeds[i];
-                    day++;
-
-                    if (pro[i] >= 100) {
-                        count++;
+        for (int mvNum : moves) {
+            for (int i = 0; i < doll[0].length; i++) {
+                if (doll[i][mvNum - 1] > 0) {
+                    if (!deque.isEmpty() && deque.peekLast() == doll[i][mvNum - 1]) {
+                        count += 2;
+                        deque.removeLast();
+                        doll[i][mvNum - 1] = 0;
+                        break;
+                    } else {
+                        deque.addLast(doll[i][mvNum - 1]);
+                        doll[i][mvNum - 1] = 0;
+                        break;
                     }
-
-
                 }
-            } else {
-                count++;
             }
-
         }
-        list.add(count);
-
-
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
-
-        System.out.println(list);
-
-
-        return result;
+        return count;
     }
 }
