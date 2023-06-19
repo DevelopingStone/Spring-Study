39d76f74afef10e7db171c8208ddcd875fd6e89c
Knight <kangrack333@gmail.com>
2023-06-19 12:42:53 +0900

    2023-06-19

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -6,13 +6,16 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Solution user = new Solution();
+        Solution user = new Solution(0);
 
         int[] pri = {1, 3, 2, 2, 4, 3};
         int location = 3;
 
-        System.out.println(user.solution(pri, location));
+//        System.out.println(user.solution(pri, location));
 
+
+
+        user.solution(10);
 
     }
 }
@@ -20,42 +23,27 @@ public class Main {
 
 
 
-class Solution {
-    public int solution(int[] priorities, int location) {
-        int answer = 0;
-        int l = location;
+class Solution{
 
-        Queue<Integer> que = new LinkedList<Integer>();
-        for(int i : priorities){
-            que.add(i);
-        }
+    int top = -1;
+    int[] arraySize ;
 
-
-        Arrays.sort(priorities);
-        int size = priorities.length-1;
-
-
-        
-
-
-
-        while(!que.isEmpty()){
-            System.out.println("que = " + que);
-            Integer i = que.poll();
-            if(i == priorities[size - answer]){
-                System.out.println("priorities[size - answer] = " + priorities[size - answer]);
-                answer++;
-                l--;
-                if(l <0)
-                    break;
-            }else{
-                que.add(i);
-                l--;
-                if(l<0)
-                    l=que.size()-1;
-            }
-        }
-
-        return answer;
+    public Solution(int array) {
+        arraySize = new int[array];
+        this.top = top;
     }
+
+    public void solution(int value){
+
+        if(top== arraySize.length-1){
+            System.out.println("Arrays size is zero, Please size add up");
+            return ;
+        }
+
+        arraySize[++top] = value;
+
+        System.out.println("arraySize[0] = " + arraySize[0]);
+        
+    }
+    
 }
