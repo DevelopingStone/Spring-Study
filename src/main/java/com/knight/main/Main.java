21d4e4913a37bee252161aa4b2ed44a9f41d73ff
Knight <kangrack333@gmail.com>
2023-06-23 00:04:56 +0900

    2023-06-23

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -5,20 +5,35 @@ public class Main {
     public static void main(String[] args) {
         Solution user = new Solution();
 
-        System.out.println(user.factorial(5));
+        int[] list = {2, 4};
+        int[] reserve = {1, 3, 5};
+        String[] babbling = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        System.out.println(user.solution(babbling));
+
 
     }
 }
 
 
 class Solution {
+    public int solution(String[] babblings) {
+        // "aya", "ye", "woo", "ma" 4가지 발음만 가능
+        int answer = 0;
+        for (int i = 0; i < babblings.length; i++) {
+            if (babblings[i].contains("ayaaya") || babblings[i].contains("yeye") || babblings[i].contains("woowoo") || babblings[i].contains("mama")) {
+                continue;
+            }
+            babblings[i] = babblings[i].replace("aya", " ");
+            babblings[i] = babblings[i].replace("ye", " ");
+            babblings[i] = babblings[i].replace("woo", " ");
+            babblings[i] = babblings[i].replace("ma", " ");
+            babblings[i] = babblings[i].replace(" ", "");
 
-    public int factorial(int value){
-        if(value<=2){
-            return 1;
+            if ("".equals(babblings[i])) answer++;
+
+
         }
 
-        return factorial(value-1) + factorial(value-2);
+        return answer;
     }
-
 }
