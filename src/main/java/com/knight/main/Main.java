377bb37f770f8073c5d0c8a2f36bd755731444ce
Knight <kangrack333@gmail.com>
2023-07-04 00:22:08 +0900

    2023-07-04

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -1,11 +1,14 @@
 package com.knight.main;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution user = new Solution();
+        int[] arr = {1, 1, 1, 2, 2, 3, 3, 4};
 
-        System.out.println(user.solution("cdcd"));
+        System.out.println(user.solution2(6, arr));
 
 //        StringBuilder sb = new StringBuilder("baabaa");
 //        sb.delete(1, 3);
@@ -17,22 +20,95 @@ public class Main {
 
 
 class Solution {
-    public int solution(String s) {
-        StringBuilder sb = new StringBuilder(s);
+    public int solution(int k, int[] tangerine) {
+        Arrays.sort(tangerine);
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        int max = 0;
+        int result = 0;
+        int count = 0;
+        for (int nums : tangerine) {
+            int value = map.getOrDefault(nums, 0);
+            map.put(nums, value + 1);
 
-        for (int i = 0; i < sb.length() - 1; i++) {
-            if (sb.charAt(i) == sb.charAt(i + 1)) {
-                sb.delete(i, i + 2);
-                i = -1;
+            if (max < value + 1) {
+                max = value + 1;
+            }
+        }
+        System.out.println(map);
 
+
+        while (true) {
+            for (Map.Entry<Integer, Integer> mapEntry : map.entrySet()) {
+
+                if (mapEntry.getValue() >= max) {
+                    result += mapEntry.getValue();
+                    count++;
+                    if (result >= k) {
+                        return count;
+                    }
+                }
+
+            }
+            max--;
+            count = 0;
+            result = 0;
+
+        }
+    }
+
+    public int solution2(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for (int t : tangerine) {
+            m.put(t, m.getOrDefault(t, 0) + 1);
+        }
+
+        System.out.println(m);
+
+        List<Integer> list = new ArrayList<>(m.values());
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list);
+
+        for (Integer a : list) {
+            k -= a;
+            answer++;
+            if (k < 1) {
+                return answer;
             }
         }
 
-        if (sb.length() == 0) {
-            return 1;
-        } else {
-            return 0;
+        return answer;
+    }
+
+
+    public static int solution3(int k, int[] tangerine) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
 
+        int sum = 0;
+        int ans = 0;
+        List<Integer> list = new ArrayList<>();
+        for (Integer x : map.keySet()) {
+            list.add(map.get(x));
+        }
+        list.sort(Collections.reverseOrder());
+
+        for (Integer x : list) {
+            ans++;
+            sum += x;
+            if (sum >= k) {
+                break;
+            }
+        }
+
+        return ans;
     }
 }
+
+
+
+
