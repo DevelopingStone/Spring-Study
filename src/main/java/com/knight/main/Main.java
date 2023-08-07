2d2ccdc36acb5a6f975c404ff4f0193df3dff939
Knight <kangrack333@gmail.com>
2023-08-07 21:45:24 +0900

    2023-08-06

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -1,10 +1,12 @@
 package com.knight.main;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(new Solution().solution(80, new int[][]{{80, 20}, {50, 40}, {30, 10}}));
+        System.out.println(new Solution().solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
 
 
     }
@@ -12,25 +14,24 @@ public class Main {
 
 
 class Solution {
-    public static boolean check[];
-    public static int ans = 0;
+    public int solution(int[] scoville, int K) {
+        int result = 0;
+        Arrays.sort(scoville);
+        while (scoville[0] <= K) {
+            scoville[0] = scoville[0] + (scoville[1] * 2);
+            scoville[1] = Integer.MAX_VALUE;
 
-    public int solution(int k, int[][] dungeons) {
-        check = new boolean[dungeons.length];
+            result++;
+            Arrays.sort(scoville);
 
-        dfs(k, dungeons, 0);
-
-        return ans;
-    }
-
-    public static void dfs(int tired, int[][] dungeons, int cnt) {
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!check[i] && dungeons[i][0] <= tired) {
-                check[i] = true;
-                dfs(tired - dungeons[i][1], dungeons, cnt + 1);
-                check[i] = false;
+            if (scoville[0] == Integer.MAX_VALUE) {
+                break;
             }
+
+
         }
-        ans = Math.max(ans, cnt);
+
+
+        return result;
     }
 }
