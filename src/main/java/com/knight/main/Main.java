bb23d8311502c00aed5ffb6cbd7f021ce63e8236
Knight <kangrack333@gmail.com>
2023-08-15 20:43:40 +0900

    2023-08-15

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -21,6 +21,8 @@ public class Main {
             com1 = random.nextInt(10);
             com2 = random.nextInt(10);
             com3 = random.nextInt(10);
+            
+            
 
             if (!(com1 == com2 || com2 == com3 || com3 == com1)) {
                 extractNum += com1 * 100 + com2 * 10 + com3;
@@ -30,6 +32,10 @@ public class Main {
 
         }
 
+        System.out.println("com1 = " + com1);
+        System.out.println("com2 = " + com2);
+        System.out.println("com3 = " + com3);
+
 
         System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
 
@@ -38,7 +44,27 @@ public class Main {
 
             System.out.printf("%d번째 시도 : ", challenge);
             int myNum = scan.nextInt();
+            user1 = myNum / 100;
+            myNum -= user1 * 100;
+            user2 = myNum / 10;
+            myNum -= user2 * 10;
+            user3 = myNum;
+
             challenge++;
+            System.out.println("user1 = " + user1);
+            System.out.println("user2 = " + user2);
+            System.out.println("user3 = " + user3);
+
+
+            if(com1==user1){
+                S++;
+            }
+            else if (com1==user2){
+                B++;
+            }
+            else if(com1 ==user3){
+                B++;
+            }
 
 
         }
