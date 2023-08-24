1212aa9d41605688d31fe4afdd4c28bc35ccdef4
Knight <kangrack333@gmail.com>
2023-08-24 12:52:19 +0900

    2023-08-24

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -6,28 +6,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(new Solution().solution(10, 2));
+        Boolean[][] arr = new Boolean[][]{{}};
 
+        System.out.println(new Solution().solution(new int[][]{{5, 7, 1}, {2, 3, 5}}, new int[][]{{5, 1, 6}, {7, 5, 6}}, new Boolean[][]{{true, true, false}, {false, true, false}}));
     }
 }
 
 
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int result[] = new int[2];
+    public int[][] solution(int[][] arr1, int[][] arr2, Boolean[][] signs) {
+        int rows = arr1.length;   // 행의 개수를 가져옴
+        int cols = arr1[0].length; // 열의 개수를 가져옴
+        int[][] answer = new int[rows][cols]; // 결과를 담을 배열 생성
 
-
-        if (brown > yellow) {
-            result[0] = yellow + 2;
-            result[1] = (brown + yellow) / result[0];
-        } else {
-
+        for (int i = 0; i < rows; i++) { // 행을 순회
+            for (int j = 0; j < cols; j++) { // 열을 순회
+                // signs 배열의 값에 따라 덧셈 또는 뺄셈 수행하여 결과 배열에 저장
+                answer[i][j] = signs[i][j] ? arr1[i][j] + arr2[i][j] : -(arr1[i][j] + arr2[i][j]);
+            }
         }
 
-        System.out.println(Arrays.toString(result));
+        for (int[] a : answer) {
+            System.out.println(Arrays.toString(a));
+        }
 
 
-        return result;
+        return answer; // 결과 배열 반환
     }
 }
 
