59267fb80cdd8a9f68696f6d27f4df1c2d04ac93
Knight <kangrack333@gmail.com>
2023-08-29 22:45:38 +0900

    2023-08-28

--- src/main/java/com/knight/main/Main01.java
+++ src/main/java/com/knight/main/Main01.java
@@ -4,17 +4,39 @@ public class Main01 {
 
     public static void main(String[] args) {
 
-        System.out.println(new Solution01().solution("He11oWor1d", "lloWorl", 2));
+        System.out.println(new Solution01().solution(new String[]{".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."}, 2));
+
 
     }
 }
 
 
 class Solution01 {
-    public String solution(String my_string, String overwrite_string, int s) {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        StringBuilder sb = new StringBuilder();
+        int position = 0;
+        int num = k;
 
-        StringBuilder sb = new StringBuilder(my_string);
-        sb.replace(s, overwrite_string.length() + s, overwrite_string);
-        return sb.toString();
+
+        for (String str : picture) {
+            num = k;
+
+            while (num > 0) {
+                for (int i = 0; i < str.length(); i++) {
+                    for (int j = 0; j < k; j++) {
+                        sb.append(str.charAt(i));
+                    }
+
+                }
+                answer[position++] = sb.toString();
+                sb.setLength(0);
+                num--;
+            }
+
+        }
+
+
+        return answer;
     }
 }
