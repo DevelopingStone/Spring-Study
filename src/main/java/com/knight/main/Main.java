dc8f4865d8631bc57f748dd2a767a12e92327313
Knight <kangrack333@gmail.com>
2023-09-20 15:05:24 +0900

    2023-09-20

--- src/main/java/com/knight/main/Main.java
+++ src/main/java/com/knight/main/Main.java
@@ -1,14 +1,10 @@
 package com.knight.main;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Boolean[][] arr = new Boolean[][]{{}};
-
-        System.out.println(new Solution().solution(new int[][]{{5, 7, 1}, {2, 3, 5}}, new int[][]{{5, 1, 6}, {7, 5, 6}}, new Boolean[][]{{true, true, false}, {false, true, false}}));
+        System.out.println(new Solution().solution(2, 5));
 
 
     }
@@ -16,33 +12,17 @@ public class Main {
 
 
 class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        int num = 0;
+        for (int i = 1; i <= n; i++) {
 
-    public int[][] solution(int[][] arr1, int[][] arr2, Boolean[][] signs) {
+            answer[num++] = (long) i * x;
 
-        int[][] result = new int[arr1.length][arr1[0].length];
 
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1[0].length; j++) {
-                int value = arr1[i][j] + arr2[i][j];
-
-                if (signs[i][j]) {
-                    result[i][j] = value;
-                } else {
-                    result[i][j] = -value;
-                }
-
-            }
         }
 
 
-        for (int[] a : result) {
-            System.out.println(Arrays.toString(a));
-        }
-
-
-        return result;
+        return answer;
     }
-
 }
-
-
