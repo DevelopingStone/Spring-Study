d2769e1849a3ce1c9d9e081f7f67edf8bf6a36f5
Knight <kangrack333@gmail.com>
2023-10-12 13:50:08 +0900

    2023-10-12

---
+++ src/main/java/com/knight/d1012/Coding0101.java
@@ -0,0 +1,12 @@
+package com.knight.d1012;
+
+public class Coding0101 {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+class Solution0101 {
+
+}
---
+++ src/main/java/com/knight/d1012/Coding0104.java
@@ -0,0 +1,16 @@
+package com.knight.d1012;
+
+public class Coding0104 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
+
+
+class Solution0104 {
+
+}
