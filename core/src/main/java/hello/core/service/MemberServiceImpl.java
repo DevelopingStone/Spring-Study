6f656ccb25f926bbf2ef7e160f613772ba988c32
Knight <kangrack333@gmail.com>
2023-10-17 23:20:06 +0900

    23-10-17

---
+++ core/src/main/java/hello/core/service/MemberServiceImpl.java
@@ -0,0 +1,12 @@
+package hello.core.service;
+
+import hello.core.repository.MemberRepository;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+}
