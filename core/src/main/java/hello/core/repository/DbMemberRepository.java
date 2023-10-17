39dc59a6e89aadfcc9252a10f7be2151cd7041f2
Knight <kangrack333@gmail.com>
2023-10-18 02:43:05 +0900

    23-10-18

--- core/src/main/java/hello/core/repository/DbMemberRepository.java
+++ core/src/main/java/hello/core/repository/DbMemberRepository.java
@@ -1,4 +1,15 @@
 package hello.core.repository;
 
+import hello.core.member.Member;
+
 public class DbMemberRepository implements MemberRepository{
+    @Override
+    public void save(Member member) {
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return null;
+    }
 }
--- core/src/main/java/hello/core/service/MemberServiceImpl.java
+++ core/src/main/java/hello/core/service/MemberServiceImpl.java
@@ -1,12 +1,27 @@
 package hello.core.service;
 
+import hello.core.member.Member;
 import hello.core.repository.MemberRepository;
+import hello.core.repository.MemoryMemberRepository;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository
+          = new MemoryMemberRepository();
 
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+
+
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+
     }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
 }
