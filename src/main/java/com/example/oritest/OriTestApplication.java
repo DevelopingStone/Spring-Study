090eb0b492e5637dcebdb4c77bdb5c60a036ed1f
Stone <kangrack333@gmail.com>
2024-01-05 20:47:29 +0900

    feat : 컨트롤러 조회,추가, 삭제 기능 추가

--- src/main/java/com/example/oritest/OriTestApplication.java
+++ src/main/java/com/example/oritest/OriTestApplication.java
@@ -1,14 +1,24 @@
 package com.example.oritest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Random;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class OriTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OriTestApplication.class, args);
+//        SpringApplication.run(OriTestApplication.class, args);
 
+        Random rand = new Random();
+        StringBuilder authKey = new StringBuilder();
+        StringBuilder num = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            String ranAuthKey = Integer.toBinaryString(rand.nextInt(3));
+            String ranNum = Integer.toString(rand.nextInt(10));
+            authKey.append(ranAuthKey);
+            num.append(ranNum);
+        }
+        System.out.println(authKey);
+        System.out.println(num);
     }
 
 }
